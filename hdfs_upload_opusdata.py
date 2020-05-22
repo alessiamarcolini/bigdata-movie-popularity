3559654c13e67d3d16547ac476e708918cddbfed
Alessia <98marcolini@gmail.com>
2020-05-22 20:56:38 +0000

    Modify local and hdfs paths
    Co-authored-by: Anna Bertani <annabertani5@gmail.com>

--- hdfs_upload_opusdata.py
+++ hdfs_upload_opusdata.py
@@ -2,6 +2,6 @@ from hdfs import InsecureClient
 
 client_hdfs = InsecureClient('http://localhost:50070', user='hadoop')
 
-local_path = 'opusdata.csv'
+local_path = 'data/opusdata.csv'
 
-uploaded_path = client_hdfs.upload(hdfs_path='/', local_path=local_path)
+uploaded_path = client_hdfs.upload(hdfs_path='/raw/opusdata.csv', local_path=local_path)
