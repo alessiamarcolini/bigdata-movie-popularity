9670aff56897a853031e594bc68a863282241a4c
Alessia <98marcolini@gmail.com>
2020-06-01 23:17:32 +0000

    Use Airflow for pipeline management

---
+++ airflow_pipeline.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+project_folder = "/home/utente/bigdata-movie-popularity"
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(2),
+    "email": ["alessia.marcolini@studenti.unitn.it"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "bigdata-movie_popularity",
+    default_args=default_args,
+    description="Movie popularity",
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id="preprocess_omdb",
+    bash_command=f"python {project_folder}/movie_popularity/preprocessing_opusdata_omdb.py",
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id="rf_train",
+    depends_on_past=True,
+    bash_command=f"python {project_folder}/movie_popularity/opusdata_ombd_rf_train_baseline.py",
+    dag=dag,
+)
+
+t1 >> t2
